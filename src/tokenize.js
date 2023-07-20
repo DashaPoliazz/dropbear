4fb02b2d4316eac3c77db31e7a5e7d27426be9cc
DashaPoliazz <dasha.poliazzz@gmail.com>
2023-07-20 22:07:34 +0300

    Lexical analysis logic has been added

--- src/tokenize.js
+++ src/tokenize.js
@@ -6,6 +6,75 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+const tokenize = (input) => {
+  const tokens = [];
+  let cursor = 0;
+
+  while (cursor < input.length) {
+    const character = input[cursor];
+
+    if (isParenthesis(character)) {
+      tokens.push({
+        type: 'Parenthesis',
+        value: character,
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(character)) {
+      cursor++;
+      continue;
+    }
+
+    if (isNumber(character)) {
+      let number = character;
+
+      while (isNumber(input[++cursor])) {
+        number += input[cursor];
+      }
+
+      tokens.push({
+        type: 'Number',
+        value: Number(number),
+      });
+      continue;
+    }
+
+    if (isLetter(character)) {
+      let string = character;
+
+      while (isLetter(input[++cursor])) {
+        string += input[cursor];
+      }
+
+      tokens.push({
+        type: 'Name',
+        value: string,
+      });
+      continue;
+    }
+
+    if (isQuote(character)) {
+      let string = '';
+
+      while (!isQuote(input[++cursor])) {
+        string += input[cursor];
+      }
+
+      tokens.push({
+        type: 'String',
+        value: string,
+      });
+
+      cursor++;
+      continue;
+    }
+
+    throw new Error(`character ${character} is not valid.`);
+  }
+
+  return tokens;
+};
 
 module.exports = { tokenize };
